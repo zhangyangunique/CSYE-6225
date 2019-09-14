7ce1df96fdf71a9f76c5a26fea6d1835d7b44949
Yang <zhangyang.uestc.uog@gmail.com>
2019-09-13 23:27:34 -0700

    assignment1

---
+++ assignment1/assignment1.js
@@ -0,0 +1,33 @@
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'us-east-2'});
+// Create SQS service object
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+// Replace with your accountid and the queue name you setup
+const accountId = '075659148988';
+const queueName = 'test';
+const queueUrl = `https://sqs.us-east-2.amazonaws.com/${accountId}/${queueName}`;
+// Setup the receiveMessage parameters
+const params = {
+  QueueUrl: queueUrl,
+  MaxNumberOfMessages: 1,
+  VisibilityTimeout: 0,
+  WaitTimeSeconds: 0
+};
+sqs.receiveMessage(params, (err, data) => {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    if (!data.Message) { 
+      console.log('Nothing to process'); 
+      return;
+    }
+    const orderData = JSON.parse(data.Messages[0].Body);
+    console.log('Order received', orderData);
+    // orderData is now an object that contains order_id and date properties
+    // Lookup order data from data storage
+    // Execute billing for order
+    // Update data storage
+    // Now we must delete the message so we don't handle it again
+  }                                                                                                                                              });
